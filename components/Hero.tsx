22a67a7054a79992a52dbec94cdc70d11ef9b555
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2023-12-10 17:16:56 +1100

    feat/hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Cursor, Typewriter, useTypewriter } from "react-simple-typewriter";
 
-type Props = {}
+type Props = {};
 
 const Hero = (props: Props) => {
-  return (
-    <div>Hero</div>
-  )
-}
+  const [text, count] = useTypewriter({
+    words: ["Hi, The Name's Cameron H.", "Guy-who-loves-Next.js", "<ButLovesToCodeMore/>"],
+    loop: true,
+    delaySpeed: 2000,
+  });
 
-export default Hero
+  return (
+    <div>
+      <span>{text}</span>
+      <Cursor cursorColor="#F7AB0A" />
+    </div>
+  );
+};
+
+export default Hero;
