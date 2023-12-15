1990029df69d2d686804961d731830adc1f1a4d0
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2023-12-15 22:37:46 +1100

    feat/experience card skills

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
@@ -6,7 +7,7 @@ type Props = {};
 
 const ExperienceCard = (props: Props) => {
   return (
-    <article>
+    <article className="flex ">
       <motion.img
         initial={{
           opacity: 0,
@@ -24,11 +25,33 @@ const ExperienceCard = (props: Props) => {
         alt=""
       />
 
-      <div>
-        <h4>Full-stack developer</h4>
-        <p>Cameron Huang</p>
-        <div>{/* a */}</div>
-        <p>Start work... -Ended</p>
+      <div className="px-0 md:px-10 ">
+        <h4 className="text-4xl font-light">Full-stack developer</h4>
+        <p className="font-bold text-2xl mt-1">Cameron Huang</p>
+        <div className="flex space-x-2 my-2">
+          <Image
+            className="h-10 w-10 rounded-full"
+            src="https://cdn.sanity.io/images/ltuexkre/production/2a67945990f9c2ef568cf7e8483cla8174556463-500x500.png"
+            width={200}
+            height={200}
+            alt=""
+          />
+          <Image
+            className="h-10 w-10 rounded-full"
+            src="/Cam_selfie.jpg"
+            width={200}
+            height={200}
+            alt=""
+          />
+          <Image
+            className="h-10 w-10 rounded-full"
+            src="/Cam_selfie.jpg"
+            width={200}
+            height={200}
+            alt=""
+          />
+        </div>
+        <p className="uppercase py-5 text-gray-300">Start work... -Ended</p>
 
         <ul>
           <li>Summary points</li>
