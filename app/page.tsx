fc77998eb2a3d5f46a8d44bcbd77693fafa10315
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2024-02-15 23:27:28 +1100

    feat/project section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import About from "@/components/About";
 import Experience from "@/components/Experience";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import { NextPage } from "next";
 
@@ -21,12 +22,14 @@ const Home: NextPage = () => {
       <section id="experience" className="snap-center">
         <Experience />
       </section>
-      {/* Skills */}
+
       <section id="skills" className="snap-start">
         <Skills />
       </section>
 
-      {/* Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
       {/* Contact Me */}
     </main>
