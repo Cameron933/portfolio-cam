eb94868ef1983f0ae39f001cf1441ff611f767cc
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2024-02-27 00:41:37 +1100

    feat/form

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
 
 type Props = {};
 
@@ -13,9 +14,39 @@ const ContactMe = (props: Props) => {
           <span className="decoration-[#F7AB0A]/50 underline">Let&apos;s Talk.</span>
         </h4>
 
-        <div className="flex ">
-          <h4></h4>
+        <div className="space-y-10 flex flex-col">
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">+61 0434772771</p>
+          </div>
+
+          <div className="flex items-center space-x-5 justify-center">
+            <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">huangchu103@gmail.com</p>
+          </div>
+
+          <div className="flex items-center space-x-5 justify-center">
+            <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">Melbourne, VIC</p>
+          </div>
         </div>
+
+        <form className="flex flex-col space-y-2 w-fit mx-auto">
+          <div className="flex space-x-2">
+            <input placeholder="Name" className="contactInput" type="text" />
+            <input placeholder="Email" className="contactInput" type="text" />
+          </div>
+
+          <input placeholder="Subject" className="contactInput" type="text" />
+
+          <textarea placeholder="Message" className="contactInput" />
+          <button
+            type="submit"
+            className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
