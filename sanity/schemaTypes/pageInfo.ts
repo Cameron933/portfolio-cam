16da05112e41ca0495f675bcfdfb34c06a9f0e94
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2024-02-29 23:55:28 +1100

    update/ creating DB schema for page information

---
+++ sanity/schemaTypes/pageInfo.ts
@@ -0,0 +1,73 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'pageInfo',
+  title: 'pageInfo',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'sting',
+    },
+    {
+      name: 'role',
+      title: 'Role',
+      type: 'sting',
+    },
+    {
+      name: 'heroImage',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'backgroundInformation',
+      title: 'BackgroundInformation',
+      type: 'string',
+    },
+    {
+      name: 'profilePic',
+      title: 'ProfilePic',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'phoneNumber',
+      title: 'PhoneNumber',
+      type: 'sting',
+    },
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'sting',
+    },
+    {
+      name: 'address',
+      title: 'Address',
+      type: 'sting',
+    },
+    {
+      name: 'socials',
+      title: 'Socials',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'social'}}],
+    },
+  ],
+
+  // preview: {
+  //   select: {
+  //     title: 'title',
+  //     author: 'author.name',
+  //     media: 'mainImage',
+  //   },
+  //   prepare(selection) {
+  //     const {author} = selection
+  //     return {...selection, subtitle: author && `by ${author}`}
+  //   },
+  // },
+})
