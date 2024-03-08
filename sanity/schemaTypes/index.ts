ff1f932c69550518545e70b2a8fb4328313a0148
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2024-03-08 23:58:00 +1100

    define schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,3 +1,13 @@
-import post from './pageInfo'
+import {createSchema} from 'sanity'
+import { Schema } from 'sanity'
 
-export const schemaTypes = [post]
+import experience from './experience'
+import pageInfo from './pageInfo'
+import project from './project'
+import skill from './skill'
+import social from './social'
+
+export const createSchema = {
+  name: 'default',
+  types: [experience, pageInfo, project, skill, social],
+}
