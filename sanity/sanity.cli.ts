2ec2ed08994dd10fca8e2489c278cc37f1ab2211
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2024-03-09 21:58:24 +1100

    fix build error

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,8 +1,9 @@
+'use client'
 import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'ei25pmha',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
