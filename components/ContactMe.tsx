21c68a7c33c6b5c139ebe62948d220d304b3a25c
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2024-03-13 01:31:37 +1100

    contact debug

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -18,33 +18,38 @@ const ContactMe = (props: Props) => {
   };
 
   return (
-    <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">Contact</h3>
+    <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-4 sm:px-10 justify-evenly mx-auto items-center">
+      <h3 className="absolute top-20 md:top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Contact
+      </h3>
 
-      <div className="flex flex-col space-y-10">
-        <h4 className="text-4xl font-semibold text-center">
+      <div className="flex flex-col space-y-4 md:space-x-10 border border-yellow-500 p-10">
+        <h4 className="text-xl md:text-4xl font-semibold text-center">
           I have got just what you need.{" "}
           <span className="decoration-[#F7AB0A]/50 underline">Let&apos;s Talk.</span>
         </h4>
 
-        <div className="space-y-10 flex flex-col">
+        <div className="space-y-2 md:space-y-6 lg:space-y-10 flex flex-col">
           <div className="flex items-center space-x-5 justify-center">
             <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
-            <p className="text-2xl">+61 0434772771</p>
+            <p className="text-lg md:text-2xl">+61 0434772771</p>
           </div>
 
           <div className="flex items-center space-x-5 justify-center">
             <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
-            <p className="text-2xl">huangchu103@gmail.com</p>
+            <p className="text-lg md:text-2xl">huangchu103@gmail.com</p>
           </div>
 
           <div className="flex items-center space-x-5 justify-center">
             <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
-            <p className="text-2xl">Melbourne, VIC</p>
+            <p className="text-lg md:text-2xl">Melbourne, VIC</p>
           </div>
         </div>
 
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-2 w-fit mx-auto">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2 w-fit mx-auto px-15 sm:px-0 border border-red-500"
+        >
           <div className="flex space-x-2">
             <input {...register("name")} placeholder="Name" className="contactInput" type="text" />
             <input
