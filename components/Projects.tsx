678524d18f132aba95e37fd7eacc180ea8f4f3cd
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2024-03-26 00:25:34 +1100

    update project info

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,31 +5,107 @@ import { motion } from "framer-motion";
 type Props = {};
 
 const Projects = (props: Props) => {
-  const projects = [1, 2, 3, 4, 5];
   const projectsData = [
     {
       id: 1,
       projectName: "CourtCanva",
       projectImg: "/CourtCanva.png",
-      description: "",
+      description:
+        "Developed for a cutting-edge platform, CourtCanva enables customers to design, visualize, and order custom sports courts directly from their homes. Intensive development experience on a mono-repository and microservice architecture project including web apps for clients, franchisees, and admin portals. Collaborated in an 8-person Agile team to analyze business logic, create technical designs from product requirements, manage sprint planning and retrospectives, and delegate tasks into manageable Jira cards. Conducted peer reviews, debugging, and code revisions. Documented technical skills and solutions and fostered pair programming with new team members.",
       url: "https://www.courtcanva.com/",
-      tech: [],
+      tech: [
+        "TypeScript",
+        "Next.js",
+        "React.js",
+        "Tailwind",
+        "Storybook",
+        "SpringBoot",
+        "Yup",
+        "JWT",
+        "PostgreSQL",
+        "Jest",
+        "Junit5",
+        "AWS",
+      ],
     },
     {
       id: 2,
-      projectName: "",
-      projectImg: "",
-      description: "",
-      url: "",
-      tech: [],
+      projectName: "TunesSpire",
+      projectImg: "/TunesSpire.png",
+      description:
+        "A music streaming platform providing seamless song playback, tailored music suggestions, subscription choices, and an extensive library of music videos. Through continuous optimization of the code, as well as the use of SSR, SSG, ISR and other different means of rendering, SEO optimization, etc. to ensure that the program performance to meet the user experience.",
+      url: "https://www.tunesspire.com/",
+      tech: [
+        "TypeScript",
+        "Next.js",
+        "React.js",
+        "Tailwind",
+        "Storybook",
+        "Node.js",
+        "Express.js",
+        "Supabase",
+        "Vercel",
+      ],
     },
     {
       id: 3,
-      projectName: "",
-      projectImg: "",
-      description: "",
-      url: "",
-      tech: [],
+      projectName: "A-comosus",
+      projectImg: "/git.jpg",
+      description:
+        "A-comosus is a project designed as a clone of Linktree, aimed at allowing users to create a personalized and easy-to-navigate landing page that houses multiple links. This concept is particularly useful for individuals and businesses looking to share various online resources or social media profiles through a single link. A-comosus seeks to replicate Linktree’s core functionality while possibly introducing additional features or improvements to enhance user experience and interaction.",
+      url: "https://github.com/A-Comosus",
+      tech: [
+        "TypeScript",
+        "Next.js",
+        "React.js",
+        "Tailwind",
+        "Node.js",
+        "Express.js",
+        "GraphQL",
+        "Prisma",
+        "JWT",
+        "SendGrid",
+        "AWS",
+      ],
+    },
+    {
+      id: 4,
+      projectName: "Airbnb Clone",
+      projectImg: "/AirBnb.png",
+      description:
+        "A simple airbnb clone. It is intended to demonstrate the different rendering methods of Next.js such as SSR, SSG, ISR, etc. and simple TypeScript generic, responsive design, etc.",
+      url: "https://github.com/Cameron933/airbnb_V2",
+      tech: [
+        "TypeScript",
+        "Next.js",
+        "React.js",
+        "Tailwind",
+        "Node.js",
+        "Express.js",
+        "Sanity.io",
+        "Google Map API",
+        "Vercel",
+      ],
+    },
+    {
+      id: 5,
+      projectName: "YulongFu",
+      projectImg: "/git.jpg",
+      description:
+        "A restaurant homepage based on the ASP.NET framework. Includes login registration, email authentication, and third-party login. Users can learn about the restaurant's menu, make reservations through the calendar, and navigate to the restaurant through an interactive map.",
+      url: "https://github.com/Cameron933/Resturant_app",
+      tech: [
+        "javaScript",
+        "Moment.js",
+        "Bootstrap",
+        "C#",
+        "ASP.NET",
+        "SQL",
+        "Google Calendar",
+        "jQuery",
+        "SendGrid",
+        "Google Map API",
+      ],
     },
   ];
   return (
@@ -48,9 +124,9 @@ const Projects = (props: Props) => {
       </h3>
 
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]">
-        {projects.map((project, i) => (
+        {projectsData.map((item) => (
           <div
-            key={i}
+            key={item.id}
             className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
           >
             <motion.img
@@ -63,27 +139,22 @@ const Projects = (props: Props) => {
                 duration: 1.2,
               }}
               viewport={{ once: true }}
-              src="https://cdn.sanity.io/images/ltuexkre/production/af7ca99b5a796d0698cf9121a4a0795b5022b6be-666x375.png"
-              alt=""
+              src={item.projectImg}
+              alt={`Project Img of ${item.projectName}`}
             />
 
             <div className="space-y-2 md:space-y-4 lg:space-y-10 px-0 md:px-10 max-w-6xl">
               <h4 className="text-xl md:text-4xl font-semibold text-center">
                 <span className="underline decoration-[#F7AB0A]/50">
-                  Case Study {i + 1} of {projects.length}:
+                  Case Study {item.id + 1} of {projectsData.length}:
                 </span>{" "}
-                UPS clone
+                {item.projectName}
               </h4>
 
               <div className="flex items-center justify-center space-x-2"></div>
 
-              <p className="text-lg text-center md:text-left">
-                Hi, I&apos;m Cameron Huang, an IT professional specializing in full-stack
-                development with a passion for creating solutions that make a significant impact. I
-                have a diverse set of skills that range from JavaScript, TypeScript, and Python to
-                front-end technologies such as ReactJS and Next.js, and back-end technologies like
-                Spring Boot
-              </p>
+              <p className="text-lg text-center md:text-left">{item.description}</p>
+
             </div>
           </div>
         ))}
