2ddfbb151b96dd895e658e6fd594001b4df62894
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2024-03-26 00:55:23 +1100

    responsive fix

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -78,8 +78,17 @@ const Projects = (props: Props) => {
             className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
           >
             <motion.div
-              whileHover={{ scale: 1.05 }}
-              className="relative flex items-center justify-center w-2/5 h-auto"
+              initial={{
+                y: -100,
+                opacity: 0,
+              }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{
+                duration: 1.2,
+              }}
+              viewport={{ once: true }}
+              whileHover={{ scale: 1.1 }}
+              className="relative flex items-center justify-center w-1/3 h-auto"
               onClick={() => openProjectUrl(item.url)}
             >
               <motion.img
@@ -106,7 +115,7 @@ const Projects = (props: Props) => {
             </motion.div>
 
             <div className="space-y-2 md:space-y-4 lg:space-y-10 px-0 md:px-10 max-w-6xl">
-              <h4 className="text-xl md:text-4xl font-semibold text-center">
+              <h4 className="text-lg md:text-2xl font-semibold text-center">
                 <span className="underline decoration-[#F7AB0A]/50">
                   Case Study {item.id} of {projectsData.length}:
                 </span>{" "}
@@ -115,7 +124,7 @@ const Projects = (props: Props) => {
 
               <div className="flex items-center justify-center space-x-2"></div>
 
-              <p className="text-lg text-center md:text-left">
+              <p className="text-sm md:text-base lg:text-lg text-center md:text-left">
                 Tech Stack: {item.tech}
                 <br />
                 <br />
